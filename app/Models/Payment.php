e45583b4494dfa855b908da15b3d1e847b9eab92
Santiago1221C <111029303+Santiago1221C@users.noreply.github.com>
2025-03-13 22:18:17 -0500

    feature/payment.php-model
    Here I add payment model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -1,10 +1,73 @@
-<?php
+<?php  
 
-namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+namespace App\Http\Controllers;  
 
-class Payment extends Model
-{
-    //
+use App\Models\Payment;  
+use Illuminate\Http\Request;  
+use Illuminate\View\View;  
+
+class PaymentController extends Controller  
+{  
+    public function index(): View  
+    {  
+    $payments = Payment::all();  
+    $viewData = [];  
+    $viewData['title'] = 'Lista de Pagos'; // Agrega un título  
+    $viewData['subtitle'] = 'Pagos realizados'; // Agrega un subtítulo  
+    $viewData['payments'] = $payments; // Asegúrate de pasar los pagos  
+
+    return view('payment.index')->with('viewData', $viewData); // Cambiado a payment.index  
+    }
+
+    public function create(): View  
+    {  
+        $viewData = [];
+        $viewData['title'] = '';
+        $viewData['subtitle'] = '';
+        return view('payment.create')->with('viewData', $viewData);
+    }  
+
+    public function store(Request $request)  
+    {  
+        $request->validate([  
+            'method' => 'required|string',  
+            'status' => 'required|string',  
+            'order' => 'required|string'  
+        ]);  
+
+        Payment::create([  
+            'method' => $request->method,  
+            'status' => $request->status,  
+            'order' => $request->order  
+        ]);  
+
+        return redirect()->route('payment.index')->with('success', 'Payment created correctly.');  
+    }  
+
+    public function destroy(String $id)  
+    {  
+         
+        $payment = Payment::find($id);  
+
+        
+        if (!$payment) {  
+            return redirect()->route('payment.index')->with('error', 'Payment has not been found.');  
+        }  
+
+          
+        $payment->delete();  
+
+         
+        return redirect()->route('payment.index')->with('success', 'Payment deleted correctly');  
+    }  
+
+    public function show(String $id)  
+    {  
+        $payment = Payment::findOrFail($id);
+
+        $viewData = [];
+        $viewData ['payments'] = $payment;
+        return view('payment.show')->with('viewData', $viewData);
+    }
 }
