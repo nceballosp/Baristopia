02355dc53d67774dd22574916a6ce90b2e0f086e
nceballosp <nataliaceballospos@gmail.com>
2025-03-13 19:10:09 -0500

    feature/product-controller-model-migration

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class ProductController extends Controller
 {
-    //
+    public function index(): View
+    {
+        $viewData = [];
+        $viewData['products'] = Product::all();
+
+        return view('product.index')->with('viewData', $viewData);
+    }
+
+    public function show(string $id): View
+    {
+        $viewData = [];
+        $product = Product::findOrFail($id);
+        $viewData['product'] = $product;
+
+        return view('product.show')->with('viewData', $viewData);
+    }
+
+    public function delete(Request $request): RedirectResponse
+    {
+        $id = $request->input('id');
+        Product::destroy($id);
+
+        return redirect()->route('product.index');
+    }
+
+    public function create(): View
+    {
+        return view('product.create');
+    }
+
+    public function save(Request $request): RedirectResponse
+    {
+        Product::validate($request);
+        Product::create($request->only(['name', 'price', 'description', 'image', 'stock']));
+
+        return redirect()->route('product.create')->with('success', 'Product created successfully');
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,88 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /** 
+     * PRODUCT ATTRIBUTES
+     * $this->attributes['id'] - int - contains the product's primary key (id)
+     * $this->attributes['name'] - string - contains the product's name
+     * $this->attributes['description'] - string - contains the products's description 
+     * $this->attributes['price'] - int contains the product's price
+     * $this->attributes['image'] - string - contains the products's image 
+     * $this->attributes['stock'] - int contains the product's stock
+    */
+    
+    public static function validate($request): void
+    {
+        $request -> validate([
+            'name'=> 'required',
+            'price' => 'required | gt:0',
+        ]);
+    }
+
+    protected $fillable = ['name', 'price', 'description', 'image', 'stock'];
+
+    public function getId(): int
+    {
+        return $this->attributes['id'];
+    }
+
+    public function getName(): string
+    {
+        return $this->attributes['name'];
+    }
+
+    public function setName(string $name): void
+    {
+        $this->attributes['name'] = $name;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->attributes['description'];
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->attributes['description'] = $description;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->attributes['price'];
+    }
+
+    public function setPrice(int $price): void
+    {
+        $this->attributes['price'] = $price;
+    }
+
+    public function getImage(): string
+    {
+        return $this->attributes['image'];
+    }
+
+    public function setImage(string $image): void
+    {
+        $this->attributes['image'] = $image;
+    }
+
+    public function getStock(): int
+    {
+        return $this->attributes['stock'];
+    }
+
+    public function setStock(int $stock): void
+    {
+        $this->attributes['stock'] = $stock;
+    }
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->attributes['created_at'];
+    }
+
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->attributes['updated_at'];
+    }
 }
