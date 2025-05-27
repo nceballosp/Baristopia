839579ad3bea3fe99116658d45f144b78c1ebdb0
jjvasquezg <juanjos137k@gmail.com>
2025-05-26 23:03:02 -0500

    fix/images-bug

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -13,7 +13,7 @@
 <div class="card mb-3">
   <div class="row g-0">
     <div class="col-md-4">
-      <img src="{{ $viewData['product']->getImage()) }}" class="img-fluid rounded-start">
+      <img src="{{ asset('storage/' . $viewData['product']->getImage()) }}" class="img-fluid rounded-start">
     </div>
     <div class="col-md-8">
       <div class="card-body">
@@ -24,7 +24,7 @@
         <p class="card-text">{{ __('messages.price') }}{{ $viewData['product']->getPrice()}}</p>
         <p class="card-text">{{ __('messages.stock') }} {{ $viewData['product']->getStock()}}</p>
         @if ($viewData['product']->getStock() != 0)    
-        <form action="{{ route('cart.add', $viewData['product']->getId())  }}" method="POST">
+        <form action="{{ route('cart.add', $viewData['product']->getId()  }}" method="POST">
           @csrf
           <label id="quantity-label" for="quantity">{{ __('messages.quantity') }}</label>
           <input type="number" name="quantity" id="quantity" min="1" max="{{ $viewData['product']->getStock() }}" value="1" required> 
